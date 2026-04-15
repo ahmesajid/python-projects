cfac51a35a2bfcb857bba4f64d5ea91769f324a8
Ahmer Bin Sajid <ahmerbinsajid07@gmail.com>
2026-04-16 03:21:11 +0500

    added project weatherChecker.py to check weather of any city using wttr.in api

---
+++ weatherChecker.py
@@ -0,0 +1,22 @@
+import requests,sys
+
+#city=input("Enter City : ")
+city=sys.argv[1]
+#TAKES SECOND ARG FROM CONSOLE INPUT 
+
+API_KEY="b821c6b740917ba3bfc841e932771429"
+#url=f"http://api.openweathermap.org/data/2.5/weather?q=Lahore&appid={API_KEY}"
+url2=f"https://wttr.in/{city}/?format=j1" #J1 JSON FORMAT
+
+getData=requests.get(url2).json()
+getTempData=getData["current_condition"][0]
+
+print( "**************************************************")
+print( f"************** WEATHER IN {city.upper()} ***************")
+print( "**************************************************")
+print(f"Feels like  : {getTempData['FeelsLikeC']}")
+print(f"Humidity    : {getTempData['humidity']}")
+print(f"Temperature : {getTempData['temp_C']} (deg C)")
+print(f"Weather     : {getTempData['weatherDesc'][0]['value']}")
+print(f"Wind speed  : {getTempData['windspeedKmph']} kmph")
+print( "**************** WEATHER REPORT ******************")
