9b1c3f6a0f7e8861919dd6fd56302a15e3af299d
Ahmer Bin Sajid <ahmerbinsajid07@gmail.com>
2026-04-15 06:18:05 +0500

    main.py updated

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+import datetime
+
 print("Hello from PythonAnywhere deployment!")
 print("CI/CD Pipeline is working..")
+print(f"Execution time : {datetime.datetime.now()}")
+print("Code pulled successfully!")
+print("Code deployed on python anywhere!")
